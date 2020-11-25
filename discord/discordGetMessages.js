edaf43d9490fd8d55c21becab4c535b44b717f1b
kevinbischof <kevin.bischof@smail.th-koeln.de>
2020-11-25 22:24:13 +0100

    Create branch

--- discord/discordGetMessages.js
+++ discord/discordGetMessages.js
@@ -3,27 +3,28 @@ module.exports = function(RED) {
     const Discord = require('discord.js');
     const client = new Discord.Client();
 
-    function LowerCaseNode(config) {
+    function DiscordGetMessages(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         var token = RED.nodes.getNode(config.token);
+
+        client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}!`)
+        });
+
+        client.login(token);
+
+        client.on('message', message => {
+            message.channel.send('nachricht');
+        });
 
         node.on('input', function(msg) {
 
             //Set node status aktive
             node.status({fill:"green", shape:"dot", text:"ready"});
 
-            client.on('ready', () => {
-                console.log(`Logged in as ${client.user.tag}!`)
-            });
 
-            client.on('message', message => {
-                msg.payload = message.toString();
-                node.send(msg);
-            });
-
-            client.login(token);
         });
     }
-    RED.nodes.registerType("discordGetMessages",LowerCaseNode);
+    RED.nodes.registerType("discordGetMessages",DiscordGetMessages);
 };
